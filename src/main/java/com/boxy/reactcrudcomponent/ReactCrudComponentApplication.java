ac46a3155e30487f89dd7e9caa9cfd8c6f5a6fb2
bxy <bxy_2000@163.com>
2019-09-28 22:18:10 +0800

    Initial commit

---
+++ src/main/java/com/boxy/reactcrudcomponent/ReactCrudComponentApplication.java
@@ -0,0 +1,13 @@
+package com.boxy.reactcrudcomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactCrudComponentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactCrudComponentApplication.class, args);
+    }
+
+}
